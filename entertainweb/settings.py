858a0be4f5c2a8f093bd49dc8178d26b476a4a92
Michael Manis <electricpyrotechnic@gmail.com>
2017-08-12 09:32:35 -0400

    created www directory for the (hopefully) final fix of staticfiles

--- entertainweb/settings.py
+++ entertainweb/settings.py
@@ -135,6 +135,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 STATIC_URL = '/static/'
-# noinspection PyUnresolvedReferences
-STATIC_ROOT = '/var/www/michaelmanis.com/static/'
