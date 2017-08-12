2ae4eed1b100653b0079fcac0afd2a519f793b5e
Michael Manis <electricpyrotechnic@gmail.com>
2017-08-12 10:25:16 -0400

    added localhost to allowed hosts

--- entertainweb/settings.py
+++ entertainweb/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = [
     'entertainweb-dev.us-east-2.elasticbeanstalk.com',
     'awseb-e-z-awsebloa-t1p62mramvnf-1430633512.us-east-2.elb.amazonaws.com',
     '127.0.0.1',
+    'localhost',
 ]
 
 # Application definition
