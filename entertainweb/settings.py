a74bb903e1569c3cae0e7c6d97e21cf69d0dc0b0
Michael Manis <electricpyrotechnic@gmail.com>
2017-08-14 01:01:38 -0400

    re-instated polls for to find SSL socket error

--- entertainweb/settings.py
+++ entertainweb/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls.apps.PollsConfig',
     'lights.apps.LightsConfig',
 ]
 
